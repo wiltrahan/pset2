e66388a9c7a1a15bcabb498e0cf8d349e7cfd4ea
Wil Trahan <wiltrahan@intracitygeeks.org>
2017-10-26 18:07:39 +0000

    complete initials

--- initials/initials.c
+++ initials/initials.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(void) {
 
     string user = get_string();
-    char userFirstInitial = user[0];
+    int nameLength = strlen(user);
 
-    printf("%c\n", userFirstInitial);
+    for(int i = 0; i < nameLength; i++) {
+        if(i == 0) {
+            //always print first initial(uppercase)
+            printf("%c", toupper(user[i]));
+        } else if(user[i] == ' ') {
+            //if space, print the next char(uppercase)
+            printf("%c", toupper(user[i + 1]));
+        }
+    }
+    //loop ends, print new line
+     printf("\n");
 }
